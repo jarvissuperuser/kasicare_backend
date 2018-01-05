ee1b3acc8e1f01ed4cfc4c696ad8170dfa2050fc
Timothy T Mugadza <mugadzatt001@gmail.com>
2018-01-05 08:49:06 +0200

    kasicare

--- classes/controller.php
+++ classes/controller.php
@@ -1,11 +1,78 @@
 <?php
 class Controller{
+    public $tbl;
+    public $cols;
+    public $list_size;
+    public $db;
     public function __construct()
+    {
+        $list_size = 5;
+    }
+    public function set()
     {
         
     }
-    public function parse()
+    public function add()
+    {
+
+    }
+    public function get_all($pointer = 0)
+    {
+        $db = $this->db;
+        $tbl=$this->tbl;
+        try {
+            $qry1 = $db->slct("*", $tbl[0]," 1=1 LIMIT $pointer,$this->list_size");
+            $stmt = $db->transaction($qry1);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $exc) {
+            return ['exception'=>$exc->getTraceAsString()];
+        }
+    }
+    public function get($pointer)
+    {
+        $db =  $this->db;
+        $tbl = $this->tbl;
+        $res = [];
+        $qry = $db->slct('*', $tbl[0], "id='$pointer'");
+        $setup = $db->transaction($qry);
+        $setup->execute();
+        if ($setup->errorCode() == "0000"){
+            $res = $setup->fetchAll(PDO::FETCH_ASSOC);
+            return $res;
+        }else{
+            return ["msg"=>$setup->errorInfo(),"sql"=>$qry];
+        }
+    }
+    public function update($pointer,$col = null)
+    {
+        $db = $this->db;
+        $tbl = $this->tbl;
+        $cols = $col==null?$this->cols:$col;
+        $sorted = valuesToString($cols);
+        $qry = $db->update($tbl, $sorted, "id='$pointer'");
+        $stmt = $db->transaction($qry);
+        $stmt->execute();
+        echo json_encode(["msg"=>$stmt->errorInfo(),
+                        "why"=>$stmt->errorCode()]);
+    }
+    public function delete(Type $var = null)
     {
         # code...
+    }
+    public function valuate($vals,$cols){
+        foreach($cols as $d){
+                array_push($vals,filter_input(INPUT_POST, $d,
+                                FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+            }
+            return $vals;
+    }
+    public function valuesToString($cols){
+        $what ="";
+        foreach($cols as $d){
+            $what .= $d . "='".filter_input(INPUT_POST, $d,
+                            FILTER_SANITIZE_FULL_SPECIAL_CHARS)."',";
+        }
+        return substr($what,0 ,strlen($what)-1);
     }
 }

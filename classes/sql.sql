e9aebc56ca70f8aaaafeee94bd7390e590b3fcdd
Timothy T Mugadza <mugadzatt001@gmail.com>
2018-01-05 07:59:57 +0200

    auto_increment primary keys

--- classes/sql.sql
+++ classes/sql.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS `kasicare`;
 
 CREATE TABLE IF NOT EXISTS `kasicare`.`user_list` (
-	`id` integer NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	`name` VARCHAR(30),
 	`surname` VARCHAR(30),
 	`email` VARCHAR(70),
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `kasicare`.`user_list` (
 );
 
 CREATE TABLE IF NOT EXISTS `kasicare`.`institution` (
-	`id` integer NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	`name` varchar(40) NOT NULL,
 	`longitute` float,
 	`latitude` float,
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS `kasicare`.`institution` (
 );
 
 CREATE TABLE IF NOT EXISTS `kasicare`.`access_log` (
-	`id` integer NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	`client_id` integer NOT NULL,
 	`platform` integer NOT NULL,
 	`when` datetime NOT NULL,
@@ -31,13 +31,13 @@ CREATE TABLE IF NOT EXISTS `kasicare`.`access_log` (
 );
 
 CREATE TABLE IF NOT EXISTS `kasicare`.`platforms` (
-	`id` integer NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	`name` varchar(50) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `kasicare`.`activity_log` (
-	`id` integer NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	`data` varchar(200) NOT NULL,
 	`when` datetime NOT NULL,
 	`userlist_id` integer NOT NULL,
@@ -52,13 +52,13 @@ CREATE TABLE IF NOT EXISTS `kasicare`.`user_details` (
 	`address` varchar(80),
 	`work` varchar(15),
 	`ulist_id` integer NOT NULL,
-	`id` integer NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `kasicare`.`user_med_history` (
 	`institute` integer NOT NULL,
-	`id` integer NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	`date_of_visit` date NOT NULL,
 	`description` varchar(200),
 	`title` varchar(25) NOT NULL,
@@ -69,7 +69,7 @@ CREATE TABLE IF NOT EXISTS `kasicare`.`user_med_history` (
 );
 
 CREATE TABLE IF NOT EXISTS `kasicare`.`user_signatures` (
-	`id` integer NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	`ulist_id` integer,
 	`key_file` varchar(100),
 	`nat_id_number` varchar(15),
@@ -80,7 +80,7 @@ CREATE TABLE IF NOT EXISTS `kasicare`.`user_signatures` (
 );
 
 CREATE TABLE IF NOT EXISTS `kasicare`.`medical_professional_list` (
-	`id` integer NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	`licence_number` varchar(30) NOT NULL,
 	`specialisation_key` varchar(3) NOT NULL,
 	`reg_id` integer NOT NULL,
@@ -88,7 +88,7 @@ CREATE TABLE IF NOT EXISTS `kasicare`.`medical_professional_list` (
 );
 
 CREATE TABLE IF NOT EXISTS `kasicare`.`medical_specialisation` (
-	`id` integer NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	`key` varchar(3) NOT NULL,
 	`title` varchar(10) NOT NULL,
 	`description` varchar(150) NOT NULL,
@@ -96,7 +96,7 @@ CREATE TABLE IF NOT EXISTS `kasicare`.`medical_specialisation` (
 );
 
 CREATE TABLE IF NOT EXISTS `kasicare`.`medical_signatures` (
-	`id` integer NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	`ulist_id` integer NOT NULL,
 	`key_file` integer NOT NULL,
 	`expiration` date NOT NULL,
@@ -106,7 +106,7 @@ CREATE TABLE IF NOT EXISTS `kasicare`.`medical_signatures` (
 );
 
 CREATE TABLE IF NOT EXISTS `kasicare`.`relations` (
-	`id` integer NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	`user_1` integer NOT NULL,
 	`user_2` integer NOT NULL,
 	`relation_description` varchar(100),
@@ -114,7 +114,7 @@ CREATE TABLE IF NOT EXISTS `kasicare`.`relations` (
 );
 
 CREATE TABLE IF NOT EXISTS `kasicare`.`user_treatment` (
-	`id` integer NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	`ulist_id` integer NOT NULL,
 	`umhistory_id` integer,
 	`med_description` varchar(100),
@@ -125,7 +125,7 @@ CREATE TABLE IF NOT EXISTS `kasicare`.`user_treatment` (
 );
 
 CREATE TABLE IF NOT EXISTS `kasicare`.`bookings` (
-	`id` integer NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	`booking` date NOT NULL,
 	`confirmed` char,
 	`type_` char,
@@ -135,7 +135,7 @@ CREATE TABLE IF NOT EXISTS `kasicare`.`bookings` (
 );
 
 CREATE TABLE IF NOT EXISTS `kasicare`.`survey` (
-	`id` integer NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	`title` VARCHAR(40) NOT NULL,
 	`description` VARCHAR(100),
 	`ulist_id` INTEGER NOT NULL,
@@ -143,7 +143,7 @@ CREATE TABLE IF NOT EXISTS `kasicare`.`survey` (
 );
 
 CREATE TABLE IF NOT EXISTS `kasicare`.`survey_question` (
-	`id` INTEGER NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	`question` VARCHAR(250),
 	`type_` INTEGER,
 	`surv_id` INTEGER NOT NULL,
@@ -151,7 +151,7 @@ CREATE TABLE IF NOT EXISTS `kasicare`.`survey_question` (
 );
 
 CREATE TABLE IF NOT EXISTS `kasicare`.`survey_answers` (
-	`id` INTEGER NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	`surv_id` INTEGER NOT NULL,
 	`response` VARCHAR(500),
 	`ulist_id` INTEGER NOT NULL,
@@ -159,7 +159,7 @@ CREATE TABLE IF NOT EXISTS `kasicare`.`survey_answers` (
 );
 
 CREATE TABLE IF NOT EXISTS `kasicare`.`survey_question_options` (
-	`id` INTEGER NOT NULL,
+	`id` integer NOT NULL AUTO_INCREMENT, 
 	`surv_question_id` INTEGER NOT NULL,
 	`option` VARCHAR(100) NOT NULL,
 	PRIMARY KEY (`id`)

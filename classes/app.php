24ac5b7d27c1e240c3e21915f9d1e266ff9afd09
Timothy T Mugadza <mugadzatt001@gmail.com>
2018-01-07 19:45:18 +0200

    clean

--- classes/app.php
+++ classes/app.php
@@ -114,79 +114,13 @@ class App{
                 return ["not a valid option"];
         }
 		}
-    //c
-    //crud plus helpers
-    //create
     
-    //   not implememted yet
-    public  function add_article(){
-        $db = $this->db;
-        try {
-            $fn = filter_input(INPUT_POST, 'image_1');
-            $fe = filter_input(INPUT_POST, 'ext');
-            $file = substr(hash('sha256',filter_input(INPUT_POST, 'image_1')),0,60) . '.' . $fe;
-            $exp = filter_input(INPUT_POST, "exp");
-            $updata = filter_input(INPUT_POST, "data");
-            $cs = filter_input(INPUT_POST, "chcksm");
-            if ($exp < $cs && $fn != 'none'){
-                return "{\"". $fe .'":"'.$exp . "\",\"" . file_put_contents($file , 
-                                base64_decode($updata), FILE_APPEND) . "\":\"" . $cs . "\"}";
-            }
-            if ($exp >= $cs || $fn == 'none'){
-                $msg = 'saving started...';
-                $cols = ["title","main","creator","image_1","image_2","title_description"];
-                $vals  = $this->valuate([], $cols);
-                $msg .= $file . "<+= " . $fn . " names" ;
-                if ($fn != 'none'){	$vals[3] = $file; }
-                $qry1 = Q_ueryBuild::insert("newsroom.articles", $cols,$vals);
-                $stmt = $db->transaction($qry1);
-                $stmt->execute();
-                return json_encode(["msg"=>$msg . $qry1, "why"=>$stmt->errorInfo(),"extra"=>$vals]);
-            }
-        } catch (Exception $e){
-            $data = ["why"=> $e->getMessage(),"error"=>$e->getCode()];
-            return json_encode($data);
-        }
-    }
 
     //read
    
     //update
    
     //helper
-
-    public function login()
-    {
-        $db = $this->db;
-        $selection = "*";
-        $tbl = "newsroom.content_creators";//table to query
-        $email = filter_input(INPUT_POST,"email",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $what = "email='$email'";
-        $qry = $db->slct($selection, $tbl, $what);
-        $stmt = $db->transaction($qry);
-        $stmt->execute();
-        if ($stmt->errorCode()=="0000"){
-            $res = $stmt->fetch(PDO::FETCH_ASSOC);
-            $slt = "count(p_key) as password";
-            $table = "newsroom.security";//table to query
-            $p_key = hash("sha256",filter_input(INPUT_POST,"p_key",
-                            FILTER_SANITIZE_FULL_SPECIAL_CHARS));
-            $w = "cc_id='" . $res["id"] . "' and p_key='$p_key' ";
-            $qry1 = $db->slct($slt, $table, $w);
-            $stmt1 = $db->transaction($qry);
-            $stmt1->execute();
-            $r = $stmt1->rowCount();
-            if ($stmt1->errorCode()=="0000"&&$r == 1){
-                return ($res);
-            }else{
-                return (["msg"=>"error","why"=>$stmt1->errorInfo(),
-                        "extra"=>"Possible Password Mismatch 1"]);
-            }
-        }else{
-            return (["msg"=>"error","why"=>$stmt->errorInfo(),
-                        "extra"=>"Possible Password Mismatch 0"]);
-        }
-    }
     public function valuate($vals,$cols){
         foreach($cols as $d){
                 array_push($vals,filter_input(INPUT_POST, $d,
